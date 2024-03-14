d01f495f2e9d29b90bc2f70d7dd580743e13997a
oscarbfs <oscar.borgesfs@gmail.com>
2024-03-14 19:55:24 -0300

    finalizando delete class

--- google_classroom/src/app/ui/modules/class/pages/delete-class-page/delete-class-page.component.ts
+++ google_classroom/src/app/ui/modules/class/pages/delete-class-page/delete-class-page.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { ClassBusiness } from '../../../../business/class_business';
 import { AuthBusiness } from '../../../../business/auth_business';
@@ -23,46 +23,37 @@ import { DeleteClassCommand } from '../../../../../domain/models/commands/delete
 export class DeleteClassPageComponent {
   
   classId: String = "";
-  deleteClassForm: FormGroup;
   errorMessage: String | null = null;
   
   constructor(
-    private fb: FormBuilder,
     private classBusiness: ClassBusiness,
     private authBusiness: AuthBusiness,
     private dialogRef: MatDialogRef<DeleteClassPageComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any // Injetando os dados da turma
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private router: Router,
   ) {
     this.classId = data.classId;
-    
-    this.deleteClassForm = this.fb.group({
-      name: [data.className || '', Validators.required], // Use os dados da turma para preencher os campos do formulário
-      section: [data.classSection || ''],
-      discipline: [data.classDiscipline || ''],
-      room: [data.classRoom || '']
-    });
   }
 
-  async onSubmit() {
+  async onDelete() {
     this.errorMessage = null;
-    if (this.deleteClassForm.valid) {
       
-      const deleteClassCommand = new DeleteClassCommand(
-        this.classId,
-      );
+    const deleteClassCommand = new DeleteClassCommand(
+      this.classId,
+    );
 
-      try {
-        var token = await this.authBusiness.getAuthToken();
-        const result = await this.classBusiness.deleteClass(deleteClassCommand, token);
+    try {
+      var token = await this.authBusiness.getAuthToken();
+      const result = await this.classBusiness.deleteClass(deleteClassCommand, token);
 
-        if (result) {
-          this.closeForm();
-        } else {
-          throw Error("Erro ao criar turma. Por favor, tente novamente mais tarde.")
-        }
-      } catch (error: any) {
-        this.errorMessage = error.message;
+      if (result) {
+        this.closeForm();
+        this.router.navigate(['/classes']);
+      } else {
+        throw Error("Erro ao deletar turma. Por favor, tente novamente mais tarde.")
       }
+    } catch (error: any) {
+      this.errorMessage = error.message;
     }
   }
 
